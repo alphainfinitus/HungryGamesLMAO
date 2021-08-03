3f14c5cc77a412a3db8d1dd1f91798feb60319d6
Mridul Kumar <kumar.mridul000@gmail.com>
2021-08-03 21:19:37 +0530

    Refactor Code Tidier Folder structure

--- Source/HungryGamesLMAO/HungryGamesLMAOGameMode.cpp
+++ Source/HungryGamesLMAO/HungryGamesLMAOGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "HungryGamesLMAOGameMode.h"
 #include "HungryGamesLMAOHUD.h"
-#include "HungryGamesLMAOCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AHungryGamesLMAOGameMode::AHungryGamesLMAOGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
